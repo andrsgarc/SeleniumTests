e809320d0a0353a1a0fc6b3c7a4c6bc82a52df04
Andres <andresd.garciag@gmail.com>
2014-03-10 13:33:58 -0600

    iniciativa create test AG

--- SeleniumProject/main/src/main/java/cr/go/ice/medios/pages/IniciativaPage.java
+++ SeleniumProject/main/src/main/java/cr/go/ice/medios/pages/IniciativaPage.java
@@ -67,22 +67,32 @@ public class IniciativaPage {
 	
 	public void openFormIniciativa(){		
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-
+		driver.findElement(By.xpath("//span[text()='Iniciativas']")).click();
+		driver.findElement(By.xpath("//span[text()='Iniciativas']")).click();		
 		driver.findElement(By.id("listCampCampana:btnInsertar")).click();
 	    driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:txt_nombreCampana")).clear();
-	    driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:txt_nombreCampana")).sendKeys("iniciativaTestSeleniumB");
+	    driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:txt_nombreCampana")).sendKeys("iniciativaselenB2");
 	    driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:fechaInicio_cb")).click();
-	    driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:fechaInicio_text_51")).click();
+	    new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//table[@id='formIniIniciativa:tabSetIniIniciativa:0:fechaInicio_ct']")));
+	    driver.findElement(By.xpath("//table[@id='formIniIniciativa:tabSetIniIniciativa:0:fechaInicio_ct']//span[text()=15]")).click();
+	    driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:fechaInicio_cb")).click();
 	    driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:fechaFin_cb")).click();
-	    driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:fechaFin_text_1607")).click();
+	    new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//table[@id='formIniIniciativa:tabSetIniIniciativa:0:fechaFin_ct']//span[text()='25']")));
+	    driver.findElement(By.xpath("//table[@id='formIniIniciativa:tabSetIniIniciativa:0:fechaFin_ct']//span[text()=25]")).click();
 	    driver.findElement(By.id("formIniIniciativa:btnContinuar")).click();
-	    driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:cambiarSector")).click();
-	    new Select(driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:orgSector"))).selectByVisibleText("TELECOMUNICACIONES");
-	    driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:cambiarDireccion")).click();
-	    new Select(driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:orgDireccion"))).selectByVisibleText("Negocio Comunicación");
-	    driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:btnCambiarNegocio")).click();
-	    new Select(driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:somNegocio"))).selectByVisibleText("Comunicación");
-	    new Select(driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:orgProceso"))).selectByVisibleText("Negocio Comunicación");
+	    driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:fechaFin_cb")).click();
+	    new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.id("formIniIniciativa:tabSetIniIniciativa:0:cambiarSector"))); 
+	    driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:orgSector")).click();
+	    new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//option[text()='TELECOMUNICACIONES']")));
+	    fluentWait(By.id("formIniIniciativa:tabSetIniIniciativa:0:orgSector"), "OrgSector[id=50050]");
+	    driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:orgDireccion")).click();
+	    new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//option[text()='Negocio Comunicaci\u00f3n']")));
+	    fluentWait(By.id("formIniIniciativa:tabSetIniIniciativa:0:orgDireccion"), "OrgDireccion[id=52152]");
+	    driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:somNegocio")).click();
+	    new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//option[text()='Comunicaci\u00f3n']")));
+	    fluentWait(By.id("formIniIniciativa:tabSetIniIniciativa:0:somNegocio"), "NegNegocio[id=5]");
+	    new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//option[@value='OrgProceso[id=51353]' and text()='Negocio Comunicaci\u00f3n']")));
+	    fluentWait(By.id("formIniIniciativa:tabSetIniIniciativa:0:orgProceso"), "OrgProceso[id=51353]");
 	    driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:inputTextCampCampanaPresupuestoEstimado")).click();
 	    driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:inputTextCampCampanaPresupuestoEstimado")).clear();
 	    driver.findElement(By.id("formIniIniciativa:tabSetIniIniciativa:0:inputTextCampCampanaPresupuestoEstimado")).sendKeys("100000000");
@@ -90,10 +100,6 @@ public class IniciativaPage {
 	    driver.findElement(By.id("popupFormAudit:txt_observacion")).clear();
 	    driver.findElement(By.id("popupFormAudit:txt_observacion")).sendKeys("Iniciativa nueva Selenium IDE testing");
 	    driver.findElement(By.id("popupFormAudit:btnAcepptConfirmation")).click();
-	    /*try {
-	      assertEquals("", driver.findElement(By.id("listCampCampana:btnInsertar")).getAttribute("value"));
-	    } catch (Error e) {
-	      verificationErrors.append(e.toString());
-	    }	*/
+
 	}
 }
---
+++ SeleniumProject/main/src/main/java/cr/go/ice/medios/tests/IniciativaTest.java
@@ -0,0 +1,18 @@
+package cr.go.ice.medios.tests;
+
+import org.junit.Test;
+import static org.junit.Assert.assertSame;
+
+import cr.go.ice.medios.pages.IniciativaPage;
+import cr.go.ice.medios.pages.MainPage;
+import cr.go.ice.medios.pages.OrdenServicioPage;
+
+public class IniciativaTest extends TestSicome{
+
+	@Test
+	public void crearIniciativaTest() throws Exception {
+		IniciativaPage tmpIniciativaPage = new IniciativaPage(driver);
+		tmpIniciativaPage.login();
+		tmpIniciativaPage.openFormIniciativa();
+	}
+}
